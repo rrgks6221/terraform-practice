117660a8ce310501e671f30c2f18a402df0f4263
LeeSeokHo <rrgks@naver.com>
2023-07-24 18:39:48 +0900

    add application load balancer listener

--- modules/lb/alb.tf
+++ modules/lb/alb.tf
@@ -22,3 +22,14 @@ resource "aws_lb_target_group" "external" {
     "Name" = "${var.app_name_dash}-external-lb-target-group"
   }
 }
+
+resource "aws_lb_listener" "name" {
+  load_balancer_arn = aws_lb.external.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.external.arn
+    type             = "forward"
+  }
+}
