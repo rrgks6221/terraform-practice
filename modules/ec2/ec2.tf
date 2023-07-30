b34a25cf07848de79363f31ed664fd66efd8696f
LeeSeokHo <rrgks@naver.com>
2023-07-30 17:31:42 +0900

    change free tier ec2 ami ubuntu v22 to amazon linux2

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -3,17 +3,13 @@ resource "aws_eip" "ec2_eip" {
 }
 
 resource "aws_instance" "free_tier" {
-  # ubuntu v22
-  ami                    = "ami-0c9c942bd7bf113a2"
+  # Amazon linux2 ami
+  ami                    = "ami-0e4a9ad2eb120e054"
   key_name               = aws_key_pair.key_pair.key_name
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ec2_free_tier.id]
   subnet_id              = var.public_subnet_ids[0]
 
-  ebs_block_device {
-    device_name = "/dev/sda1"
-    volume_size = 30
-  }
 
   tags = {
     "Name" = "${var.app_name_dash}-public-ec2"
