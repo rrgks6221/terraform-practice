94bf1ccafffa33c769df2749db9a2d08b1dae43e
LeeSeokHo <rrgks@naver.com>
2023-08-22 14:13:39 +0900

    code build

--- modules/code_build/code_build.tf
+++ modules/code_build/code_build.tf
@@ -15,9 +15,10 @@ resource "aws_codebuild_project" "main" {
 
   # Environment
   environment {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/standard:2.0"
-    type         = "LINUX_CONTAINER"
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:2.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
 
     environment_variable {
       name  = "PORT"
